6f671e32baa3642161990c7a3b34793d5c6ad914
Yudha Pratama <yudha.webdev@gmail.com>
2021-05-27 14:23:35 +0700

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'facebook': '#385898',
+        'facebook-darker': '#314E89',
+        'twitter': '#1A94DA',
+        'twitter-darker': '#1681BF',
+        'whatsapp': '#179848',
+        'whatsapp-darker': '#0c6c33'
+      }
+    },
+    fontFamily: {
+      'body': ['Baloo 2'],
+    }
+  },
+  variants: {},
+  plugins: []
+};
