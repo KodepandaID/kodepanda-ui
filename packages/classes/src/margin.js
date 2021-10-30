e43d5f07e8d67892f50148789e3994c36e517276
Yudha Pratama <yudha.webdev@gmail.com>
2021-10-30 10:08:14 +0700

    Added support auto to margin and padding

--- packages/classes/src/margin.js
+++ packages/classes/src/margin.js
@@ -2,17 +2,17 @@ import cx from "clsx";
 
 const Margin = (mx, my, mb, ml, mr, mt) => {
   const margin = cx(
-    (mx !== undefined && mx >= 0) && `mx-${mx}`,
+    (mx !== undefined && mx >= 0 || mx === "auto") && `mx-${mx}`,
     (mx !== undefined && mx < 0) && `-mx${mx}`,
-    (my !== undefined && my >= 0) && `my-${my}`,
+    (my !== undefined && my >= 0 || my === "auto") && `my-${my}`,
     (my !== undefined && my < 0) && `-my${my}`,
-    (mb !== undefined && mb >= 0) && `mb-${mb}`,
+    (mb !== undefined && mb >= 0 || mb === "auto") && `mb-${mb}`,
     (mb !== undefined && mb < 0) && `-mb${mb}`,
-    (ml !== undefined && ml >= 0) && `ml-${ml}`,
+    (ml !== undefined && ml >= 0 || ml === "auto") && `ml-${ml}`,
     (ml !== undefined && ml < 0) && `-ml${ml}`,
-    (mr !== undefined && mr >= 0) && `mr-${mr}`,
+    (mr !== undefined && mr >= 0 || mr === "auto") && `mr-${mr}`,
     (mr !== undefined && mr < 0) && `-mr${mr}`,
-    (mt !== undefined && mt >= 0) && `mt-${mt}`,
+    (mt !== undefined && mt >= 0 || mt === "auto") && `mt-${mt}`,
     (mt !== undefined && mt < 0) && `-mt${mt}`,
   )
 
--- packages/classes/src/padding.js
+++ packages/classes/src/padding.js
@@ -2,17 +2,17 @@ import cx from "clsx";
 
 const Padding = (px, py, pb, pl, pr, pt) => {
   const padding = cx(
-    (px !== undefined && px >= 0) && `px-${px}`,
+    (px !== undefined && px >= 0 || px === "auto") && `px-${px}`,
     (px !== undefined && px < 0) && `-px${px}`,
-    (py !== undefined && py >= 0) && `py-${py}`,
+    (py !== undefined && py >= 0 || py === "auto") && `py-${py}`,
     (py !== undefined && py < 0) && `-py${py}`,
-    (pb !== undefined && pb >= 0) && `pb-${pb}`,
+    (pb !== undefined && pb >= 0 || pb === "auto") && `pb-${pb}`,
     (pb !== undefined && pb < 0) && `-pb${pb}`,
-    (pl !== undefined && pl >= 0) && `pl-${pl}`,
+    (pl !== undefined && pl >= 0 || pl === "auto") && `pl-${pl}`,
     (pl !== undefined && pl < 0) && `-pl${pl}`,
-    (pr !== undefined && pr >= 0) && `pr-${pr}`,
+    (pr !== undefined && pr >= 0 || pr === "auto") && `pr-${pr}`,
     (pr !== undefined && pr < 0) && `-pr${pr}`,
-    (pt !== undefined && pt >= 0) && `pt-${pt}`,
+    (pt !== undefined && pt >= 0 || pt === "auto") && `pt-${pt}`,
     (pt !== undefined && pt < 0) && `-pt${pt}`,
   )
 
