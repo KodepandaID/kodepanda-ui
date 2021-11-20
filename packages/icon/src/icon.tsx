ace25f06c84179a3ee0b2e59173ce5b64a8c846a
Yudha Pratama <yudha.webdev@gmail.com>
2021-11-20 14:10:35 +0700

    Rewrite icon component

---
+++ packages/icon/src/icon.tsx
@@ -0,0 +1,51 @@
+import { base, Size, SpacingProps, StandardProps, text, VisualProps } from "@zenbu-ui/core"
+import { ThemeCtx } from "@zenbu-ui/provider"
+import { Outline, OutlineKeys } from "./icon-outline"
+import * as React from "react"
+import { Solid, SolidKeys } from "./icon-solid"
+
+interface IconProps extends StandardProps, VisualProps, SpacingProps  {
+  icon: Outline | Solid,
+  height?: Size
+}
+
+export const Icon: React.FC<IconProps> = (props) => {
+  const { dark } = React.useContext(ThemeCtx)
+  const Elm = props.icon.includes("solid") ? SolidKeys[props.icon] : OutlineKeys[props.icon]
+
+  const cls = text({
+    visualText: {
+      dark: dark,
+      textColor: props.color,
+      textColorContrast: props.colorContrast,
+      textHoverColor: props.colorHover,
+      textHoverColorContrast: props.colorHoverContrast,
+      darkTextColor: props.darkColor,
+      darkTextColorContrast: props.darkColorContrast,
+      darkTextHoverColor: props.darkColorHover,
+      darkTextHoverColorContrast: props.darkColorHoverContrast
+    },
+    spacing: {
+      mx: props.mx,
+      my: props.my,
+      mb: props.mb,
+      ml: props.ml,
+      mr: props.mr,
+      mt: props.mt
+    }
+  })
+
+  const clsBase = base({
+    model: {
+      height: props.height
+    }
+  })
+
+  return(
+    <Elm className={`${clsBase} ${cls}`} />
+  )
+}
+
+Icon.defaultProps = {
+  height: 12
+}
---
+++ packages/icon/stories/Icon.stories.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Icon } from "../src"
+
+export default { title: 'Components/Icon' }
+
+export const Basic = () => {
+  return(
+    <Icon icon="academic-cap" />
+  )
+}
+
+export const Solid = () => {
+  return(
+    <Icon icon="academic-cap-solid" />
+  )
+}
+
+export const Coloring = () => {
+  return(
+    <Icon icon="academic-cap" color="blue" colorContrast={600} />
+  )
+}
