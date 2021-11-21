e6ef722ffaeb78cc476d7b594ef8bf2199cfa526
Yudha Pratama <yudha.webdev@gmail.com>
2021-11-21 11:32:23 +0700

    Update image test

--- packages/image/test/image.test.tsx
+++ packages/image/test/image.test.tsx
@@ -15,9 +15,47 @@ describe("Standard Image component", () => {
     expect(await axe(rendered.container)).toHaveNoViolations()
   })
 
-  it("should have element img and classname ", async () => {
+  it("should have element img and classname w-52", async () => {
     expect(rendered.container.firstChild).toContainHTML(
       `<img class="w-52" alt="Daniel Seßler - Free Iceland" src="${url}" />`
     )
+  })
+
+  describe("Image with caption component", () => {
+    beforeEach(() => {
+      rendered = render(<Image
+        caption="Daniel Seßler - Free Iceland"
+        border borderColor="gray" borderColorContrast={200} px={2} py={2}
+        alt="Daniel Seßler - Free Iceland" src={url} />)
+    })
+
+    it("should have no violations in default state", async () => {
+      expect(await axe(rendered.container)).toHaveNoViolations()
+    })
+
+    it("should have element figure with child img and figcaption", async () => {
+      expect(rendered.container.firstChild).toContainHTML(
+        [
+          `<figure class="flex flex-col w-max border border-solid border-gray-200 px-2 py-2">`,
+          `<img class="w-52" alt="Daniel Seßler - Free Iceland" src="${url}" />`,
+          `<figcaption class="bg-black text-white text-xs text-center px-2 py-2 italic">Daniel Seßler - Free Iceland</figcaption>`,
+          `</figure>`
+        ].join("")
+      )
+    })
+  })
+
+  describe("Image Circle component", () => {
+    beforeEach(() => {
+      rendered = render(<Image circle alt="Daniel Seßler - Free Iceland" src={url} />)
+    })
+
+    it("should have no violations in default state", async () => {
+      expect(await axe(rendered.container)).toHaveNoViolations()
+    })
+
+    it("should have element img with classname w-52 h-52 rounded-full", async () => {
+      expect(rendered.container.firstChild).toHaveClass("w-52 h-52 rounded-full")
+    })
   })
 })
