7a6a06dfbdf22ec3aa12a8e2592f869758866e51
Yudha Pratama <yudha.webdev@gmail.com>
2021-12-16 18:11:14 +0700

    Added core useHook

---
+++ packages/core/src/useHook.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: React.RefObject<T>,
+  handler: (event: Event) => void,
+) => {
+  React.useEffect(() => {
+    const listener = (event: Event) => {
+      const el = ref?.current
+      if (!el || el.contains((event?.target as Node) || null)) {
+        return
+      }
+
+      handler(event)
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [ref, handler])
+}
+
+export const useEscKeyboardEvent = <T extends HTMLElement = HTMLElement>(
+  ref: React.RefObject<T>,
+  handler: (event: Event) => void
+) => {
+  React.useEffect(() => {
+    if (ref.current !== null) {
+      const listener = (event: KeyboardEvent) => {
+        if (event.code === "Escape") {
+          handler(event)
+        }
+      }
+
+      document.addEventListener('keydown', listener)
+
+      return () => {
+        document.addEventListener('keydown', listener)
+      }
+    }
+  }, [ref, handler])
+}
