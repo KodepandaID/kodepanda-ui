6a8c5a2897b8e577744b2287955ddffa8d898239
Yudha Pratama <yudha.webdev@gmail.com>
2021-12-16 18:11:29 +0700

    Added core useKeyboard

---
+++ packages/core/src/useKeyboard.ts
@@ -0,0 +1,39 @@
+export const arrowNavigation = (
+  e: React.KeyboardEvent,
+  id: string | undefined,
+  handler: (code: string) => void
+) => {
+  const arr = id !== undefined ? id.split("-") : []
+  if (e.code === "ArrowDown" && arr.length > 0) {
+    let idx = Number(arr[arr.length - 1]) + 1
+    delete arr[arr.length - 1]
+
+    let elm = document.getElementById(`${arr.join("-")}${idx}`)
+    if (elm?.hasAttribute("data-disabled")) {
+      do {
+        idx += 1
+        elm = document.getElementById(`${arr.join("-")}${idx}`)
+      } while(elm?.hasAttribute("data-disabled"))
+    }
+
+    elm?.focus()
+    handler(e.code)
+  } else if (e.code === "ArrowUp" && arr.length > 0) {
+    let idx = Number(arr[arr.length - 1]) - 1
+
+    if (idx > 0) {
+      delete arr[arr.length - 1]
+
+      let elm = document.getElementById(`${arr.join("-")}${idx}`)
+      if (elm?.hasAttribute("data-disabled")) {
+        do {
+          idx -= 1
+          elm = document.getElementById(`${arr.join("-")}${idx}`)
+        } while(elm?.hasAttribute("data-disabled") && idx > 0)
+      }
+
+      elm?.focus()
+      handler(e.code)
+    }
+  }
+}
